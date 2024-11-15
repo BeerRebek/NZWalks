c36fb8c95735938d4f09be5eb924adc25ffa37e5
AmitSalpekar0078 <salpekaramit1999@gmail.com>
2024-11-15 20:29:02 +0530

    Created Domain Model, Configer Database and DbContext, EF Migrations

---
+++ NZWalks.API/NZWalks.API/Data/NZWalksDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDBContext : DbContext
+    {
+        public NZWalksDBContext(DbContextOptions<NZWalksDBContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Region> Regions { get; set; } //Create Regions Table if not exist in DB
+        public DbSet<Walk> Walks { get; set; } //Create Walks Table if not exist in DB
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; } //Create WalkDifficulty Table if not exist in DB
+    }
+}
---
+++ NZWalks.API/NZWalks.API/Migrations/20241115145304_Added_Long_Region.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    public partial class Added_Long_Region : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Long",
+                table: "Regions",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Long",
+                table: "Regions");
+        }
+    }
+}
